3a090e4594f44613bd8d77aee16e759e058b5b22
mandrysz <01187204@pw.edu.pl>
2024-12-12 11:29:47 +0000

    Lab4 Fixed ID3 algorithm when value was not known

--- lab4/ID3_algorithm.py
+++ lab4/ID3_algorithm.py
@@ -73,20 +73,27 @@ class ID3(Solver):
         Returns:
             dict: the decision tree
         """
+        majority_class = np.bincount(y).argmax()
+
         if len(np.unique(y)) == 1 or (self.max_depth is not None and depth >= self.max_depth):
-            return np.bincount(y).argmax()
+            # If all examples have the same class or the maximum depth is reached, return the majority class
+            return majority_class
 
+        # Find the best attribute to split
         best_feature = np.argmax([self.information_gain(X, y, i) for i in range(X.shape[1])])
-        tree = {best_feature: {}}
+        tree = {best_feature: {"majority_class": majority_class, "branches": {}}}
 
+        # Iterate through all possible values of the feature
         for value in range(int(np.max(X[:, best_feature])) + 1):
             subset_X = X[X[:, best_feature] == value]
             subset_y = y[X[:, best_feature] == value]
 
             if len(subset_y) == 0:
-                tree[best_feature][value] = np.bincount(y).argmax()
+                # If no data, assign the majority class of the current node
+                tree[best_feature]["branches"][value] = majority_class
             else:
-                tree[best_feature][value] = self.fit(subset_X, subset_y, depth + 1)
+                # Recursively build the subtree
+                tree[best_feature]["branches"][value] = self.fit(subset_X, subset_y, depth + 1)
 
         return tree
 
@@ -102,13 +109,19 @@ class ID3(Solver):
             any: the predicted class
         """
         if not isinstance(tree, dict):
+             # If leaf node, return its value
             return tree
+
         feature = list(tree.keys())[0]
+        node = tree[feature]
         value = x[feature]
-        if value in tree[feature]:
-            return self.predict_one(x, tree[feature][value])
+
+        if value in node["branches"]:
+            # If the value exists, continue recursion
+            return self.predict_one(x, node["branches"][value])
         else:
-            return max(tree[feature].values(), key=lambda v: v if not isinstance(v, dict) else -1)
+            # If the value does not exist, return the majority class of the current node
+            return node["majority_class"]
 
     def predict(self, X):
         """
